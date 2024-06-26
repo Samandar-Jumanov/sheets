63d0c3c213be71513ee71632bd84b66e98dfbc69
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-06-26 07:46:20 -0600

    Adding cron job

--- app.ts
+++ app.ts
@@ -1,32 +1,38 @@
 import express, { Express, Request, Response } from "express";
-import { compareAndUpdateSheet } from   "./utils/spreedsheet"
+import cron from "node-cron";
+import { compareAndUpdateSheet } from "./utils/spreedsheet";
 import Logger from "./lib/winston";
 import bot from "./lib/bot";
 import TelegramBot from "node-telegram-bot-api";
 import { deleteMessages, readMessages } from "./utils/file";
 import { IBotMessageInfo } from "./types";
 
-
 const app: Express = express();
-
 
 // Health checker
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Success" });
 });
 
-bot.onText(/\/start/, async (msg : TelegramBot.Message) => {
+// Telegram bot /start command handler
+bot.onText(/\/start/, async (msg: TelegramBot.Message) => {
   const chatId = msg.chat.id.toString();
-  const messages : IBotMessageInfo[] | null  = readMessages();
-  await  bot.sendMessage(chatId, "Bot has started. You will receive notifications.");
-  deleteMessages()
-  
+  const messages: IBotMessageInfo[] | null = readMessages();
+  await bot.sendMessage(chatId, JSON.stringify(messages));
+  deleteMessages();
 });
 
+// Initial execution of the compareAndUpdateSheet function
+compareAndUpdateSheet().then().catch(( error : any ) => Logger.error(error));
 
-compareAndUpdateSheet().then()
-.catch(err => Logger.error(err))
+// Schedule the compareAndUpdateSheet function to run every 15 minutes
+cron.schedule("*/15 * * * *", () => {
+  compareAndUpdateSheet()
+    .then()
+    .catch(( error : any ) => Logger.error(error));
+});
 
-app.listen(3000,  () => {
+// Start the Express server
+app.listen(3000, () => {
   Logger.info("Server started on port 3000");
 });
